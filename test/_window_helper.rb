010cae178b78d1f42be1559a49305c080438886c
Coderzthereal <real.paul.hartman@gmail.com>
2023-04-19 13:30:43 -0700

    Add delegator for Window class

---
+++ test/_window_helper.rb
@@ -0,0 +1,18 @@
+require_relative "../src/game_of_life"
+
+WindowClass, GameOfLife::Window = GameOfLife::Window, Class.new # To redefine entire class instead of adding to it
+class GameOfLife::Window
+  attr_reader :called
+  def initialize(**kwargs)
+    @called = {}
+    @window = WindowClass.new(**kwargs)
+  end
+
+  def method_missing(...) = @called[store(...)] = @window.send(...)
+
+  def respond_to_missing?(...) = @window.respond_to?(...)
+
+  private def store(first_arg = nil, *args, **kwargs, &block) = [
+    first_arg, args, kwargs, block
+  ]
+end
