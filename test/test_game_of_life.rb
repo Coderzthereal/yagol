a9dd8908d1567fe51d8795de0a824d8f886e3036
Coderzthereal <real.paul.hartman@gmail.com>
2023-04-19 13:32:20 -0700

    Refactor existing GOL test to use minitest

--- test/test_game_of_life.rb
+++ test/test_game_of_life.rb
@@ -1,28 +1,15 @@
 require "yaml"
-require_relative "../src/game_of_life"
+require "minitest/autorun"
+require_relative "_window_helper" # To have method call reporting from game.window
 
-# Stub (turn into mock)
-WindowClass, GameOfLife::Window = GameOfLife::Window, Class.new # To redefine entire class instead of adding to it
-class GameOfLife::Window
-  attr_reader :called
-  def initialize(**kwargs)
-    @called = {}
-    @window = WindowClass.new(**kwargs)
+
+class GameOfLifeTest < Minitest::Test
+  def setup
+    @test_opts = YAML.load_file("test/test_cfg.yml")
+    @game = GameOfLife.new(**@test_opts)
   end
 
-  def method_missing(...) = @called[store(...)] = @window.send(...)
-
-  def respond_to_missing?(...) = @window.respond_to?(...)
-
-  private def store(first_arg = nil, *args, **kwargs, &block) = [
-    first_arg, args, kwargs, block
-  ]
+  def test_initialize
+    assert_equal @game.all_tiles, (0...10).to_a.product((0...10).to_a)
+  end
 end
-
-opts = YAML.load_file("../cfg/main.yml")
-game = GameOfLife.new(**opts)
-
-# Just to see what we're working with
-game.show
-puts
-pp game.window.called
